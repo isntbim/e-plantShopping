5b79c835992d58669b6588b61ea2ee4fff3393af
Zhi Katherine <18861832289@163.com>
2025-05-29 11:13:05 -0700

    Initial commit

---
+++ src/store.js
@@ -0,0 +1,10 @@
+import { configureStore } from '@reduxjs/toolkit';
+import cartReducer from './CartSlice';
+ export const store = configureStore({
+    // Define the root reducer object
+    reducer: {
+        // 'cart' is the name of the slice in the store, and it's managed by cartReducer
+        cart: cartReducer,
+    },
+});
+export default store
